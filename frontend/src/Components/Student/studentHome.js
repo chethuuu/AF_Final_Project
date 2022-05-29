5bd01e70a1ee0ef2f079654d9f5bcc19a4b6743b
Chethana Prasadi <89582259+chethuuu@users.noreply.github.com>
2022-05-29 13:12:53 +0530

    Update StudentHome

--- frontend/src/Components/Student/studentHome.js
+++ frontend/src/Components/Student/studentHome.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import img1 from '../../img/img1.png'
+import img2 from '../../img/img2.png'
+import img3 from '../../img/img3.png'
+import img4 from '../../img/img4.png'
+import img5 from '../../img/img5.png'
 
 const studentHome = () => (
 
@@ -16,29 +20,27 @@ const studentHome = () => (
         <div class="row">
             <div class="col-md-4">
                 <center>
-                    <img width="300px" src={img1}  />
+                    <img width="240px" src={img3}  />
                     <h4> <a href="/createGrp"> Create Student Groups </a></h4>
                 </center>
             </div>
 
             <div class="col-md-4">
                 <center>
-                    <img width="300px" src={img1} />
+                    <img width="310px" src={img1} />
                     <h4> <a href="/viewtopic"> Register for Research Topic </a> </h4>
                 </center>
             </div>
 
             <div class="col-md-4">
                 <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="/viewtopic"> Request Co-Supervisor </a> </h4>
+                    <img width="150px" src={img5} />
+                    <h4> <a href="/viewreg"> Request Supervisor </a> </h4>
                 </center>
             </div>
-
         </div>
 
         <br /> <br />
-
         <div class="row">
             <div class="col-md-4">
                 <center>
@@ -46,26 +48,19 @@ const studentHome = () => (
                     <h4> <a href="#"> Request Co-Supervisor  </a> </h4>
                 </center>
             </div>
-
             <div class="col-md-4">
                 <center>
-                    <img width="300px" src={img1}  />
+                    <img width="220px" src={img2}  />
                     <h4> <a href="#"> Sumbit Documents </a> </h4>
                 </center>
             </div>
-
             <div class="col-md-4">
                 <center>
-                    <img width="300px" src={img1} />
+                    <img width="300px" src={img4} />
                     <h4> <a href="#"> Download Templates </a> </h4>
                 </center>
             </div>
-
             <br /> <br />
-
-
-
-
         </div>
     </div>
 
