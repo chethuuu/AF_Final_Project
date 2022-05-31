671cfb7db6378021513a59f37684938e3f72f4eb
Chethana Prasadi <89582259+chethuuu@users.noreply.github.com>
2022-05-31 20:39:28 +0530

    Add new input field

--- frontend/src/Components/Student/regResearch.js
+++ frontend/src/Components/Student/regResearch.js
@@ -8,6 +8,7 @@ const ResearchTopic = () => {
 
   const [inputs, setInputs] = useState({
     gid: "",
+    lead_no: "",
     name: "",
     interest: "",
   });
@@ -23,6 +24,7 @@ const ResearchTopic = () => {
     const res = await axios
       .post("http://localhost:5000/api/rtopics", {
         gid: inputs.gid,
+        lead_no: lead_no,
         name: inputs.name,
         interest: inputs.interest,
       })
@@ -43,30 +45,34 @@ const ResearchTopic = () => {
     <div>
       <div>
         <div className="container shadow my-5 mx-auto w-50">
-            <div className="">
-            </div>
-            <div className="col p-5 mx-auto">
-              <h1 className=" fw-bolder mb-5"><center>Register Research Topic</center></h1>
-              <form onSubmit={handleSubmit}>
-                <div class="mb-3">
-                  <label for="gid" class="form-label">Group ID</label>
-                  <input name="gid" value={inputs.gid} onChange={onChangeInput} type="text" class="form-control" id="gid" placeholder='Enter Group ID' required />
-                </div>
-                <div class="mb-3">
-                  <label for="name" class="form-label">Research Topic</label>
-                  <input name="name" value={inputs.name} onChange={onChangeInput} type="text" class="form-control" placeholder='Enter Research Topic' required />
-                </div>
-                <div class="mb-3">
-                  <label for="interest" class="form-label">Topic Category</label>
-                  <input name="interest" value={inputs.interest} onChange={onChangeInput} type="text" class="form-control" id="interest" placeholder="Eneter Research Topic Category" required />
-                </div> 
-                <p>Once you Register for a topic, you can't change it</p> <br/>
-                <button type="submit" class="btn btn-danger w-100 rounded-pill">Register Topic</button>
-                <br/>
-                <NavLink to="/viewreg"><button className='btn btn-warning w-100 rounded-pill' type="submit"> View Register Topic </button></NavLink>
-              </form>
-            </div>
-          
+          <div className="">
+          </div>
+          <div className="col p-5 mx-auto">
+            <h1 className=" fw-bolder mb-5"><center>Register Research Topic</center></h1>
+            <form onSubmit={handleSubmit}>
+              <div class="mb-3">
+                <label for="gid" class="form-label">Group ID</label>
+                <input name="gid" value={inputs.gid} onChange={onChangeInput} type="text" class="form-control" id="gid" placeholder='Enter Group ID' required />
+              </div>
+              <div class="mb-3">
+                <label for="name" class="form-label">Group Leader's IT Number</label>
+                <input name="lead_no" value={inputs.lead_no} onChange={onChangeInput}  type="text" class="form-control" placeholder='Enter Group Leaders IT Number' required />
+              </div>
+              <div class="mb-3">
+                <label for="name" class="form-label">Research Topic</label>
+                <input name="name" value={inputs.name} onChange={onChangeInput} type="text" class="form-control" placeholder='Enter Research Topic' required />
+              </div>
+              <div class="mb-3">
+                <label for="interest" class="form-label">Topic Category</label>
+                <input name="interest" value={inputs.interest} onChange={onChangeInput} type="text" class="form-control" id="interest" placeholder="Eneter Research Topic Category" required />
+              </div>
+              <p>Note:- Once you Register for a topic, you can't Research Topic it</p> <br />
+              <button type="submit" class="btn btn-danger w-100 rounded-pill">Register Topic</button>
+              <br />
+              <NavLink to="/viewreg"><button className='btn btn-warning w-100 rounded-pill' type="submit"> View Register Topic </button></NavLink>
+            </form>
+          </div>
+
         </div>
       </div>
     </div>
