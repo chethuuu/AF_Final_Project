0e3b5a0b2bd8e358fa03365fc2749909c633a349
Chethana Prasadi <89582259+chethuuu@users.noreply.github.com>
2022-06-02 02:10:25 +0530

    Add Request Filter to Backend

--- backend/routes/ResearchTopicRoute.js
+++ backend/routes/ResearchTopicRoute.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const Research_Topic = require('../models/ResearchTopicModel');
 const ResearchTopicCtrl = require('../controller/ResearchTopicCtrl');
 
 router.route('/rtopics')
@@ -8,10 +9,10 @@ router.route('/rtopics')
 router.route('/rtopicss/:id')
   .put(ResearchTopicCtrl.updateResearch_Topic)
   .delete(ResearchTopicCtrl.deleteResearch_Topic);
-  
+
 router.route('/rtopicsbyID/:id')
   .get(ResearchTopicCtrl.getResearch_TopicByID);
-  
+
 router.route('/rtopics/:gid')
   .get(ResearchTopicCtrl.getDetailsbyGroupID)
 
@@ -20,5 +21,26 @@ router.route('/rtopics/filter/:interest')
 
 router.route('/rtopics/status/:status_sup')
   .get(ResearchTopicCtrl.getDetailsbyStatus);
+
+router.route('/status/approve')
+  .get(ResearchTopicCtrl.getApproveStatus);
+
+// router.route('/status/req')
+//   .get(ResearchTopicCtrl.getEmailStatus);
+
+router.route('/status/req')
+  .get(ResearchTopicCtrl.getApproveSupStatus);
+
+router.route('/status/notreq')
+  .get(ResearchTopicCtrl.getEmailStatusNot);
+
+// router.get("/getdata/filter", async(req,res) => {
+//   try {
+//       const allgrps = await Group.find({pannel_status:'Not assign'});
+//       res.status(200).json(allgrps);
+//   } catch (err) {
+//       res.json(err);
+//   }
+// });
 
 module.exports = router;
