bef9082a8a71d49f4996d80ca1ee4275fa3663f4
Chethana Prasadi <89582259+chethuuu@users.noreply.github.com>
2022-06-02 06:21:21 +0530

    Style Supervisor

--- frontend/src/Components/Supervisor/ViewResearchTopicSup.js
+++ frontend/src/Components/Supervisor/ViewResearchTopicSup.js
@@ -47,14 +47,14 @@ function ViewResearchTopicSup() {
             <div className='container shadow my-5'>
                 <div class="input-group">
                     <div class="form-inline my-2 my-lg-0">
-                        <h5 className='grpid'>Select your Group ID</h5>
-                        <Button className='btn btn-primary search' onClick={() => { SearchItem({ selects }) }}>Search</Button>
-                        <select className='form-control select-gid' name="gid" id="gid" placeholder='Search your Group ID' value={selects} onChange={e => setSelects(e.target.value)}>
+                        <h5 className='search-topic'>Select your Group ID</h5>
+                        <Button className='btn btn-primary search-btn' onClick={() => { SearchItem({ selects }) }}>Search</Button>
+                        <select className='form-control select-btn w-50' name="gid" id="gid" placeholder='Search your Group ID' value={selects} onChange={e => setSelects(e.target.value)}>
                             <option>None</option>
                             <option>G001</option>
                             <option>G008</option>
                         </select>
-                    </div> <br /><br /><br />
+                    </div> <br />
                 </div>
 
                 <table class="table">
@@ -68,7 +68,7 @@ function ViewResearchTopicSup() {
                             <th scope='col'>Supervisor Status</th>
                             <th scope='col'>Accept / Reject</th>
                             <th scope='col'>Send E-mail</th>
-                            <th scope="col">Update</th>
+                            {/* <th scope="col">Update</th> */}
                             <th scope="col">Delete</th>
                         </tr>
                     </thead>
@@ -84,7 +84,7 @@ function ViewResearchTopicSup() {
                                     <td><button className='btn btn-danger mx-auto'>{topic.status_sup}</button></td>
                                     <td><Link to={`/updatereqsup/${topic._id}`}><Button className='btn btn-warning'>Accept / Reject</Button></Link> </td>
                                     <td><Link to={`/sendmail/${topic._id}`}><button className='btn btn-success'>Send Status to Group Leader</button></Link></td>
-                                    <td><Link to={`/updatereqq/${topic._id}`}><Button className='btn btn-danger'>Update</Button></Link> </td>
+                                    {/* <td><Link to={`/updatereqq/${topic._id}`}><Button className='btn btn-danger'>Update</Button></Link> </td> */}
                                     <td><button className='btn btn-danger' onClick={() => deleteResearch_Topic(topic._id)}>Delete</button></td>
                                 </tr>
                             )
--- frontend/src/Components/Supervisor/supHome.js
+++ frontend/src/Components/Supervisor/supHome.js
@@ -1,73 +1,77 @@
 import React from 'react';
+
 import img1 from '../../img/img1.png'
+import img10 from '../../img/img10.png'
 
-const supHome = () => (
+const supHome = () => {
+    return (
+        <div class="container my-0 py-3">
+            <div className="row">
+                <div className="col-12">
+                    <div className='col-12'>
+                        <h1 className='display-6 text-center mb-4'> <b> Supervisor DashBoard </b></h1>
+                        <hr className='w-25 mx-auto' />
+                    </div>
+                </div>
+            </div>
 
-    <div class="container">
-        <div class="row">
-            <div class="col-12">
-                <center>
-                    <h1>Dashboard</h1>
-                    <br /> <br />
-                </center>
+
+            <div class="row">
+                <div class="col-md-4">
+                    <center>
+                        <img width="300px" src={img10} />
+                        <h4> <a className='navbar-brand text-dark fs-4' href="/viewtopicsup"> View Requests </a></h4>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="300px" src={img1} />
+                        <h4> <a className='navbar-brand text-dark fs-4' href="/viewusereq"> View Research Document </a> </h4>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="300px" src={img1} />
+                        <h4> <a className='navbar-brand text-dark fs-4' href="#">  Download Marking Scheme </a> </h4>
+                    </center>
+                </div>
+            </div>
+
+            <br /> <br />
+
+            <div class="row">
+                <div class="col-md-4">
+                    <center>
+                        <img width="300px" src={img1} />
+                        <h4> <a className='navbar-brand text-dark fs-4' href="#"> Chat with Groups </a> </h4>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="300px" src={img1} />
+                        <h4> <a className='navbar-brand text-dark fs-4' href="#"> Evaluate Document </a> </h4>
+                    </center>
+                </div>
+
+                <div class="col-md-4">
+                    <center>
+                        <img width="300px" src={img1} />
+                        <h4> <a className='navbar-brand text-dark fs-4' href="#"> Inventory Management </a> </h4>
+                    </center>
+                </div>
+
+                <br /> <br />
+
+
+
+
             </div>
         </div>
+    )
 
-        <div class="row">
-            <div class="col-md-4">
-                <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="/viewtopicsup"> View Requests </a></h4>
-                </center>
-            </div>
-
-            <div class="col-md-4">
-                <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="/viewusereq"> View Research Document </a> </h4>
-                </center>
-            </div>
-
-            <div class="col-md-4">
-                <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="#">  Download Marking Scheme </a> </h4>
-                </center>
-            </div>
-        </div>
-
-        <br /> <br />
-
-        <div class="row">
-            <div class="col-md-4">
-                <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="#"> Chat with Groups </a> </h4>
-                </center>
-            </div>
-
-            <div class="col-md-4">
-                <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="#"> Evaluate Document </a> </h4>
-                </center>
-            </div>
-
-            <div class="col-md-4">
-                <center>
-                    <img width="300px" src={img1} />
-                    <h4> <a href="#"> Inventory Management </a> </h4>
-                </center>
-            </div>
-
-             <br /> <br />
-
-
-
-
-        </div>
-    </div>
-
-)
+}
 
 export default supHome;
